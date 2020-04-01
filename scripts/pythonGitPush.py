5a99268beeab5c7788eefb792aa193ca30f48659
Elias <elias.007@live.se>
2020-04-01 14:36:30 +0200

    Testing

--- scripts/pythonGitPush.py
+++ scripts/pythonGitPush.py
@@ -4,5 +4,5 @@ repo = Repo('/home/pi/Examensarbete')
 
 repo.index.add(['scripts'])
 repo.index.commit('Testing')
-origin = repo.remote('https://github.com/Dmajstrolov/python-ciscoconfig.git')
+origin = repo.create_remote('https://github.com/Dmajstrolov/python-ciscoconfig.git',repo.remotes.origin.url)
 origin.push()
