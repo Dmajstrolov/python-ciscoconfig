9196105d0938bef232d4a89d5c68a540f1ab67a0
Elias <elias.007@live.se>
2020-04-01 15:02:41 +0200

    Auto-commit

--- scripts/pythonGitPush.py
+++ scripts/pythonGitPush.py
@@ -3,6 +3,6 @@ from git import Repo
 repo = Repo('/home/pi/Examensarbete')
 
 repo.index.add(['scripts'])
-repo.index.commit('Testing')
-origin = repo.create_remote('Dmajstrolov/Automation-switch.git',repo.remotes.origin.url)
+repo.index.commit('Auto-commit')
+origin = repo.create_remote('Dmajstrolov/python-ciscoconfig.git',repo.remotes.origin.url)
 origin.push()
