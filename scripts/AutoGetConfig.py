0fd6b5cb3c4f63e85bf17cc5853619d61fb2a351
Elias <elias.007@live.se>
2020-04-02 12:29:36 +0200

    Auto-commit

---
+++ scripts/AutoGetConfig.py
@@ -0,0 +1,5 @@
+from subprocess import call
+
+call('sudo mv /srv/tftp/sw1_running_config /home/pi/Examensarbete/running-configs/', shell=True)
+
+
--- scripts/pythonGitPush.py
+++ scripts/pythonGitPush.py
@@ -2,7 +2,7 @@ from git import Repo
 
 repo = Repo('/home/pi/Examensarbete')
 
-repo.index.add(['scripts'])
+repo.index.add(['running-configs'])
 repo.index.commit('Auto-commit')
 origin = repo.create_remote('Dmajstrolov/python-ciscoconfig.git',repo.remotes.origin.url)
 origin.push()
