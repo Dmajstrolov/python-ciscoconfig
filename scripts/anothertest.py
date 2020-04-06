7d3b28315488bca908615fc0deedd1e2d3156127
Elias <elias.007@live.se>
2020-04-06 13:33:00 +0200

    Ändrat upplägget på mapparna

--- scripts/anothertest.py
+++
@@ -1,7 +0,0 @@
-from git import Repo
-repo = Repo('/home/pi/Examensarbete')
-
-repo.index.add(['running-configs'])
-repo.index.commit('New switch config')
-origin = repo.remote('https://github.com/Dmajstrolov/python-ciscoconfig.git')
-origin.push()
